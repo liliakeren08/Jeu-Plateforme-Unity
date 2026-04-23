3f57b24b7ae13ca9cbb66ec41cd297d5c4df2c93
kingred76 <wyll.jalbert@outlook.com>
2026-04-23 14:27:08 -0400

    Player

--- Assets/_MyAssets/Scripts/Player.cs
+++ Assets/_MyAssets/Scripts/Player.cs
@@ -3,8 +3,51 @@ using System;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _playerSpeed = 8f;
+    [SerializeField] private float _playerSpeed = 1f;
     public float PlayerSpeed => _playerSpeed;
     private SpriteRenderer _spriteRenderer;
-    
+    private InputSystem_Actions _inputSystem_Actions;
+    [SerializeField] private float _maxHeight = 5f;
+
+    private float _minX, _maxX;
+    private float _minY, _maxY;
+
+    private void Start()
+    {
+        Camera mainCamera = Camera.main;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        float halfPlayerWithd = _spriteRenderer.bounds.extents.x;
+        float halfPlayerHeight = _spriteRenderer.bounds.extents.y;
+        _inputSystem_Actions = new InputSystem_Actions();
+        _inputSystem_Actions.Player.Enable();
+
+        _minX = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + halfPlayerWithd;
+        _maxX = mainCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - halfPlayerWithd;
+        _minY = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + halfPlayerHeight;
+        _maxY = _maxHeight - halfPlayerHeight;
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void OnDestroy()
+    {
+        _inputSystem_Actions.Player.Disable();
+    }
+
+    private void PlayerMovement()
+    {
+        Vector2 direction2D = _inputSystem_Actions.Player.Move.ReadValue<Vector2>();
+        direction2D.Normalize();
+        transform.Translate(direction2D * Time.deltaTime * _playerSpeed);
+
+        float clampedX = Mathf.Clamp(transform.position.x, _minX, _maxX);
+        float clampedY = Mathf.Clamp(transform.position.y, _minY, _maxY);
+
+        transform.position = new Vector2(clampedX, clampedY);
+
+
+    }
 }
