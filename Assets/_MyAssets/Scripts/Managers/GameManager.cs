d71c9366be05e31c124cc6919a993455a4c64aee
kingred76 <wyll.jalbert@outlook.com>
2026-05-04 17:03:32 -0400

    go

--- Assets/_MyAssets/Scripts/Managers/GameManager.cs
+++ Assets/_MyAssets/Scripts/Managers/GameManager.cs
@@ -5,62 +5,62 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance; // Propriété statique pour accéder à l'instance du GameManager
+    public static GameManager Instance; // Propriï¿½tï¿½ statique pour accï¿½der ï¿½ l'instance du GameManager
 
-    public event EventHandler<OnEnemyDestroyedEventArgs> OnEnemyDestroyed; // Événement déclenché lorsque qu'un ennemi est détruit
+    public event EventHandler<OnEnemyDestroyedEventArgs> OnEnemyDestroyed; // ï¿½vï¿½nement dï¿½clenchï¿½ lorsque qu'un ennemi est dï¿½truit
     public class OnEnemyDestroyedEventArgs : EventArgs
     {
-        public string DestroyedObjectTag; // Propriété pour stocker le tag de l'ennemi détruit
+        public string DestroyedObjectTag; // Propriï¿½tï¿½ pour stocker le tag de l'ennemi dï¿½truit
     }
 
 
-    [Header("Gestion de la difficulté du jeu")]
+    [Header("Gestion de la difficultï¿½ du jeu")]
     [SerializeField] private float _enemySpeed = 6.0f;
-    public float EnemySpeed => _enemySpeed; // Propriété publique pour accéder à la vitesse des ennemis
-    [SerializeField] private float _speedAugmentationStep = 0.5f; // Valeur d'augmentation de la vitesse des ennemis à chaque augmentation de la difficulté
-    [SerializeField] private int _scoreThresholdForDifficultyIncrease = 500; // Seuil de score pour augmenter la difficulté du jeu
+    public float EnemySpeed => _enemySpeed; // Propriï¿½tï¿½ publique pour accï¿½der ï¿½ la vitesse des ennemis
+    [SerializeField] private float _speedAugmentationStep = 0.5f; // Valeur d'augmentation de la vitesse des ennemis ï¿½ chaque augmentation de la difficultï¿½
+    [SerializeField] private int _scoreThresholdForDifficultyIncrease = 500; // Seuil de score pour augmenter la difficultï¿½ du jeu
 
     private int _playerScore = 0; // Score du joueur
-    public int PlayerScore => _playerScore; // Propriété publique pour accéder au score du joueur
+    public int PlayerScore => _playerScore; // Propriï¿½tï¿½ publique pour accï¿½der au score du joueur
 
-    // Booléen pour indiquer si la difficulté a été augmentée, utilisé pour éviter d'augmenter la difficulté plusieurs fois pour le même seuil de score
+    // Boolï¿½en pour indiquer si la difficultï¿½ a ï¿½tï¿½ augmentï¿½e, utilisï¿½ pour ï¿½viter d'augmenter la difficultï¿½ plusieurs fois pour le mï¿½me seuil de score
     private bool _difficultyIncrease = false;
 
     private void Awake()
     {
         if (Instance == null)
         {
-            Instance = this; // Assignation de l'instance du GameManager à la propriété statique
+            Instance = this; // Assignation de l'instance du GameManager ï¿½ la propriï¿½tï¿½ statique
         }
         else
         {
-            Destroy(gameObject); // Destruction de l'objet GameManager s'il existe déjà une instance
-            Debug.LogError("Un deuxième GameManager a été créé. Assurez-vous qu'il n'y a qu'un seul GameManager dans la scène.");
+            Destroy(gameObject); // Destruction de l'objet GameManager s'il existe dï¿½jï¿½ une instance
+            Debug.LogError("Un deuxiï¿½me GameManager a ï¿½tï¿½ crï¿½ï¿½. Assurez-vous qu'il n'y a qu'un seul GameManager dans la scï¿½ne.");
         }
     }
 
     private void Start()
     {
-        PlayerPrefs.SetInt("PlayerScore", 0); // Initialisation du score du joueur dans les PlayerPrefs à 0 au démarrage du jeu
+        PlayerPrefs.SetInt("PlayerScore", 0); // Initialisation du score du joueur dans les PlayerPrefs ï¿½ 0 au dï¿½marrage du jeu
     }
 
     private void Update()
     {
-        // Vérifie si le score du joueur atteint un seuil d'augmentation de la difficulté, que le score n'est pas nul et que la difficulté n'a pas déjà été augmentée pour ce seuil
+        // Vï¿½rifie si le score du joueur atteint un seuil d'augmentation de la difficultï¿½, que le score n'est pas nul et que la difficultï¿½ n'a pas dï¿½jï¿½ ï¿½tï¿½ augmentï¿½e pour ce seuil
         if (_playerScore % _scoreThresholdForDifficultyIncrease == 0 && _playerScore != 0 && !_difficultyIncrease)
         {
-            _difficultyIncrease = true; // Marque que la difficulté a été augmentée pour ce seuil de score
+            _difficultyIncrease = true; // Marque que la difficultï¿½ a ï¿½tï¿½ augmentï¿½e pour ce seuil de score
             _enemySpeed += _speedAugmentationStep; // Augmentation de la vitesse des ennemis
         }
         else if (_playerScore % _scoreThresholdForDifficultyIncrease != 0)
         {
-            _difficultyIncrease = false; // Réinitialise le marqueur d'augmentation de la difficulté lorsque le score n'est plus à un seuil d'augmentation
+            _difficultyIncrease = false; // Rï¿½initialise le marqueur d'augmentation de la difficultï¿½ lorsque le score n'est plus ï¿½ un seuil d'augmentation
         }
     }
 
     public void EnemyDestroyed(int p_enemyPoints, string p_gameObjectTag)
     {
-        if (p_gameObjectTag == "Laser")
+        if (p_gameObjectTag == "Bullet")
         {
             _playerScore += p_enemyPoints; // Ajout des points de l'ennemi au score du joueur
         }
@@ -68,28 +68,28 @@ public class GameManager : MonoBehaviour
         OnEnemyDestroyed?.Invoke(this, new OnEnemyDestroyedEventArgs
         {
             DestroyedObjectTag = p_gameObjectTag
-        }); // Déclenchement de l'événement de destruction d'un ennemi
+        }); // Dï¿½clenchement de l'ï¿½vï¿½nement de destruction d'un ennemi
     }
 
     public void EndGame()
     {
-        // Enregistrement du score du joueur dans les PlayerPrefs à la fin du jeu
+        // Enregistrement du score du joueur dans les PlayerPrefs ï¿½ la fin du jeu
         PlayerPrefs.SetInt("PlayerScore", _playerScore);
 
-        // Vérification si une valeur de score le plus élevé existe déjà dans les PlayerPrefs
+        // Vï¿½rification si une valeur de score le plus ï¿½levï¿½ existe dï¿½jï¿½ dans les PlayerPrefs
         if (PlayerPrefs.HasKey("PlayerHighScore"))
         {
             int highScore = PlayerPrefs.GetInt("PlayerHighScore");
             if (_playerScore > highScore)
             {
-                PlayerPrefs.SetInt("PlayerHighScore", _playerScore); // Mise à jour du score le plus élevé si le score actuel est supérieur
+                PlayerPrefs.SetInt("PlayerHighScore", _playerScore); // Mise ï¿½ jour du score le plus ï¿½levï¿½ si le score actuel est supï¿½rieur
             }
         }
         else
         {
-            PlayerPrefs.SetInt("PlayerHighScore", _playerScore); // Initialisation du score le plus élevé si aucune valeur n'existe encore
+            PlayerPrefs.SetInt("PlayerHighScore", _playerScore); // Initialisation du score le plus ï¿½levï¿½ si aucune valeur n'existe encore
         }
-        PlayerPrefs.Save(); // Sauvegarde des PlayerPrefs pour s'assurer que les données sont enregistrées
+        PlayerPrefs.Save(); // Sauvegarde des PlayerPrefs pour s'assurer que les donnï¿½es sont enregistrï¿½es
 
         StartCoroutine(LoadSceneDelay());
 
@@ -97,9 +97,9 @@ public class GameManager : MonoBehaviour
 
     IEnumerator LoadSceneDelay()
     {
-        yield return new WaitForSeconds(2f); // Attente de 1 seconde avant de charger la scène de fin de jeu
-        int noScene = SceneManager.GetActiveScene().buildIndex; // Récupération de l'index de la scène actuelle 
-        SceneManager.LoadScene(noScene + 1); // Chargement de la scène de fin de jeu
+        yield return new WaitForSeconds(2f); // Attente de 1 seconde avant de charger la scï¿½ne de fin de jeu
+        int noScene = SceneManager.GetActiveScene().buildIndex; // Rï¿½cupï¿½ration de l'index de la scï¿½ne actuelle 
+        SceneManager.LoadScene(noScene + 1); // Chargement de la scï¿½ne de fin de jeu
     }
 
     public void TriggerOnEnemyDestroyed(object sender)
