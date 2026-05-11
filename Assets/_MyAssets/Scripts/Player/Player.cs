d2630f30ac541ebac5914dece770e59aebfadbcf
kingred76 <wyll.jalbert@outlook.com>
2026-05-11 09:21:59 -0400

    playerfix

--- Assets/_MyAssets/Scripts/Player/Player.cs
+++ Assets/_MyAssets/Scripts/Player/Player.cs
@@ -5,70 +5,48 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _playerSpeed = 5f;
+
     [SerializeField] private float _PlayerXpCap = 6f;
     [SerializeField] private float _PlayerCurentXp = 0f;
     [SerializeField] private float _PlayerCurentLvl = 1f;
     [SerializeField] private float _playerLife = 20f;
+
     [SerializeField] private WeaponManager _weaponManager;
     [SerializeField] private Weapons _startingWeapon;
+
+    [SerializeField] private float _maxHeight = 5f;
+    [SerializeField] private float _minHeight = -5f;
+
     private Vector2 _lastDirection = Vector2.right;
+
     public event EventHandler<OnPlayerUpEventArgs> OnPlayerUp;
     public event EventHandler OnPlayerDeath;
+
     public float PlayerCurentXp => _PlayerCurentXp;
     public float PlayerSpeed => _playerSpeed;
-    private SpriteRenderer _spriteRenderer;
-    private InputSystem_Actions _inputSystem_Actions;
-    [SerializeField] private float _maxHeight = 5f;
 
-    private float _minX, _maxX;
-    private float _minY, _maxY;
+    private InputSystem_Actions _inputSystem_Actions;
+    private PolygonCollider2D _collider;
+
+    public class OnPlayerUpEventArgs : EventArgs
+    {
+        public float newLevel;
+    }
 
     private void Start()
     {
         _weaponManager.AddWeapon(_startingWeapon);
-        Camera mainCamera = Camera.main;
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        float halfPlayerWithd = _spriteRenderer.bounds.extents.x;
-        float halfPlayerHeight = _spriteRenderer.bounds.extents.y;
+
         _inputSystem_Actions = new InputSystem_Actions();
         _inputSystem_Actions.Player.Enable();
 
-        _minX = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + halfPlayerWithd;
-        _maxX = mainCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - halfPlayerWithd;
-        _minY = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + halfPlayerHeight;
-        _maxY = _maxHeight - halfPlayerHeight;
-    }
-    public class OnPlayerUpEventArgs : EventArgs
-    {
-        public float newLevel;
+        _collider = GetComponent<PolygonCollider2D>();
     }
 
     private void Update()
     {
         PlayerMovement();
         PlayerLvlUp();
-    }
-
-    private void PlayerLvlUp()
-    {
-        if (_PlayerCurentXp >= _PlayerXpCap)
-        {
-
-            LevelUp();
-            
-            Debug.Log("Lv: " + _PlayerCurentLvl);
-
-        }
-    }
-
-    public void AddXP(float amount)
-    {
-        _PlayerCurentXp += amount;
-    }
-
-    private void OnDestroy()
-    {
-        _inputSystem_Actions.Player.Disable();
     }
 
     private void PlayerMovement()
@@ -81,11 +59,39 @@ public class Player : MonoBehaviour
         Vector3 movement = new Vector3(direction2D.x, direction2D.y, 0f);
         transform.position += movement * _playerSpeed * Time.deltaTime;
 
-        float clampedX = Mathf.Clamp(transform.position.x, _minX, _maxX);
-        float clampedY = Mathf.Clamp(transform.position.y, _minY, _maxY);
+        // ✅ bounds du collider (toujours à jour)
+        Bounds b = _collider.bounds;
+
+        float halfWidth = b.extents.x;
+        float halfHeight = b.extents.y;
+
+        Camera mainCamera = Camera.main;
+
+        float minX = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + halfWidth;
+        float maxX = mainCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - halfWidth;
+
+        // ✅ FIX demandé : limite bas + haut
+        float minY = _minHeight + halfHeight;
+        float maxY = _maxHeight - halfHeight;
+
+        float clampedX = Mathf.Clamp(transform.position.x, minX, maxX);
+        float clampedY = Mathf.Clamp(transform.position.y, minY, maxY);
 
         transform.position = new Vector2(clampedX, clampedY);
+    }
 
+    private void PlayerLvlUp()
+    {
+        if (_PlayerCurentXp >= _PlayerXpCap)
+        {
+            LevelUp();
+            Debug.Log("Lv: " + _PlayerCurentLvl);
+        }
+    }
+
+    public void AddXP(float amount)
+    {
+        _PlayerCurentXp += amount;
     }
 
     private void LevelUp()
@@ -94,10 +100,11 @@ public class Player : MonoBehaviour
 
         _PlayerXpCap += 5f;
         _PlayerCurentXp = 0f;
+
         OnPlayerUp?.Invoke(this, new OnPlayerUpEventArgs
         {
             newLevel = _PlayerCurentLvl
-        }) ;
+        });
     }
 
     public Vector2 GetLastDirection()
@@ -117,16 +124,14 @@ public class Player : MonoBehaviour
 
     private void Die()
     {
-       
-
         OnPlayerDeath?.Invoke(this, EventArgs.Empty);
         _inputSystem_Actions.Player.Disable();
 
-        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-
-
-
+    private void OnDestroy()
+    {
+        _inputSystem_Actions.Player.Disable();
+    }
 }
